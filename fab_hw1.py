3b997bcc7888ad211fdb218f916782dfa372b022
Fabian Okeke <fnokeke@gmail.com>
2016-03-13 15:29:21 -0400

    ROC curves added and code refactored

--- fab_hw1.py
+++ fab_hw1.py
@@ -1,342 +1,239 @@
 
 # coding: utf-8
 
-# In[9]:
+# In[10]:
 
 import random
 import os.path
+import pandas as pd
+import numpy as np
+import csv
+
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+#from pyspark.sql import SQLContext, Row
+#from pyspark.sql.types import *
+
+get_ipython().magic(u'matplotlib inline')
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+
+# In[2]:
 
 def exists(filepath):
     return os.path.exists(filepath)
 
 
-# # Split 48 million rows: 10 million(training) & 38 million(testing)
+# # Split 48 million rows to 10 million(training) & 38 million(testing)
 
-# In[10]:
-
-get_ipython().system(u'wc -l train.txt')
-
-
-# In[12]:
-
-get_ipython().system(u'split -l 4775064 train.txt newfiles')
-
-
-# In[2]:
+# In[3]:
 
 if (exists('train10M.txt') and exists('test38M.txt')):
     print "train10M.txt and test38M.txt already created before."
 else:
-    ftr = open('train10M.txt', 'a')
-    fte = open('test38M.txt', 'a')
-
-    count = 0
-    with open('train.txt') as f:
-        for line in f:
-            if count <= 10000000:
-                ftr.write(line)
-                count += 1
-            else:
-                fte.write(line)
-    ftr.close()
-    fte.close()    
+    get_ipython().system(u'split -l 2000000 train.txt ff')
+    get_ipython().system(u'cat ffaa ffat ffaf ffaq ffaj > train10M.txt')
+    get_ipython().system(u'rm ffaa ffat ffaf ffaq ffaj')
+    get_ipython().system(u'cat ff* > test38M.txt')
+    get_ipython().system(u'rm ff*')
     print "Newly created files: train10M.txt and test38M.txt."
 
 
-# # Split training data into 3 partitions
+# # Split train10M data into 3 partitions
 
-# In[3]:
+# In[4]:
 
 if (exists('train5M.txt') and 
     exists('validation2M.txt') and 
     exists('test3M.txt')):
     print "train5M.txt, validation2M.txt, test3M.txt already created before."
-    
 else:
-    ftr = open('train5M.txt', 'a')
-    fva = open('validation2M.txt', 'a')
-    fte = open('test3M.txt', 'a')
-
-    with open('train10M.txt') as f:
-        for line in f:
-            value = random.randint(1,100)
-            if value <= 50:
-                ftr.write(line)
-            elif value <= 70:
-                fva.write(line)
-            else:
-                fte.write(line)
-
-    ftr.close()
-    fva.close()
-    fte.close()
+    get_ipython().system(u'split -l 1000000 train10M.txt ff')
+    get_ipython().system(u'cat ffaa ffaj ffad ffaf ffah > train5M.txt    ')
+    get_ipython().system(u'cat ffai ffae > validation2M.txt')
+    get_ipython().system(u'cat ffac ffag ffab > test3M.txt')
+    get_ipython().system(u'rm ff*')
     print "Newly created files: train5M.txt, validation2M.txt, test3M.txt."
 
 
-# # Create subsets of partitions for debug purposes
+# # Plot histograms and store images
 
-# In[4]:
+# In[19]:
 
-def create_subset(finput, foutput, noOfRows):
-    count = 0
-    fout = open(foutput, 'a')
+for col in range(1,40):
+    filepath = 'histo-file/f%d.txt' % col
+    imgpath = 'histo-img/f%d.png' % col
+    title = 'feature %d' % col
+
+    histo_df = pd.read_csv(filepath, header=None, names=["val", "freq"])
+    ax = histo_df.plot(x='val', y='freq', logy='True', title=title, xlim=(0), figsize=(25,10))
+    fig = ax.get_figure()
+    fig.savefig(imgpath)
+print "Histogram plot completed!"
+
+
+# # Select categorical features that should be kept out of all 26 possible cases
+
+# In[20]:
+
+selected_categ_cols = [14,15,18,19,21,22,26,27,30,32,33,35,36,38]
+len(selected_categ_cols)
+
+
+# # Normalize integer cols and compute rate-value on categorical cols
+
+# In[24]:
+
+def get_train_xy(mdf, selected_categorical_cols):
+    """
+        normalize integer columns of dataframe and perform weighted freq count on selected categorical columns
+        @param mdf: massive/large dataframe to be operated on (cols 1-13 are assumed to be integer columns)
+        @param selected_categ_cols: hashed string columns to be converted to float values
+        @return X: dataframe same as mdf but with 1 less column than mdf
+        @return y: 1D array
+    """
+    int_cols = range(1,14)
+    cols_to_keep = int_cols + selected_categorical_cols  
+    y = mdf[0].values
+    X = mdf[cols_to_keep].fillna(0)
+    print "X and y extracted..."
     
-    with open(finput) as fin:
-        for line in fin:
-            while count < noOfRows:
-                fout.write(line)
-                count += 1
-    fout.close()
-
-
-# In[5]:
-
-if (exists('debug_train5M.txt') and 
-    exists('debug_validation2M.txt') and 
-    exists('debug_test3M.txt')):
-    print "debug_train5M.txt, debug_validation2M.txt, debug_test3M.txt already created before."
+    # normalize integers features
+    X[int_cols] = X[int_cols].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+    print "X normalization complete..."
     
-else:
-    create_subset('train5M.txt', 'debug_train5M.txt', 10000)
-    create_subset('validation2M.txt', 'debug_validation2M.txt', 10000)
-    create_subset('test3M.txt', 'debug_test3M.txt', 10000)
-    print "Newly created files: debug_train5M.txt, debug_validation2M.txt, debug_test3M.txt."
+    # compute weighted freq: each column entry is freq/cumulative_freq
+    cum_freq = len(mdf) * 1.0
+    for col in selected_categorical_cols:
+        freq = m_count_distinct(X[col].values)
+        X[col] = X[col].apply(lambda x: freq.get(x)/cum_freq) 
+    print "Data preparation complete!"
+
+    return X, y
+
+def m_count_distinct(col):
+    """
+        count distinct values in all columns
+        @param col: numpy 1D array
+        @return: dictionary of value, freq
+    """
+    d = {}
+    for x in list(col):
+        if type(x) == np.float64 and np.isnan(x):
+            k = 'nan-' + col
+            d[k] = d.get(k, 0) + 1
+        else:
+            d[x] = d.get(x, 0) + 1
+    return d
+
+def score_higher_freq(y):
+    """
+        accuracy score for baseline method that always predicts the value with higher frequency
+        @param y: 1D array
+        @return: float value between 0.0 and 1.0
+    """
+    d = {}
+    for i in y:
+        d[i] = d.get(i,0) + 1
+    return max(d.values())/float(len(y))
 
 
-# # Load test data
+# # Prepare training and validation data
 
-# In[6]:
+# In[25]:
 
-import pandas as pd
-test_data = pd.read_table('test3M.txt')
-print test_data.shape
-test_data.head()
+df_train = pd.read_table('train5M.txt', header=None)
+X_train, y_train = get_train_xy(df_train, selected_categ_cols)
 
 
-# In[8]:
+# In[26]:
 
-get_ipython().magic(u'store test_data')
-
-
-# # Process training data
-
-# In[9]:
-
-df = pd.read_table('train5M.txt')
-print "df shape:", df.shape
+df_va = pd.read_table('validation2M.txt', header=None)
+X_va, y_va = get_train_xy(df_va, selected_categ_cols)
 
 
-# In[10]:
+# # Compute baseline score that always predicts value with higher freq
 
-get_ipython().magic(u'store df')
+# In[27]:
 
-
-# # Show column names
-
-# In[14]:
-
-print "There are %d columns." % len(df.columns.values)
-df.columns.values
+print "Baseline score:", score_higher_freq(y_va)
 
 
-# In[15]:
-
-df.head()
-
-
-# # Rename columns
-
-# In[41]:
-
-new_col_names = ['f' + str(num) for num in range(len(df.columns.values))]
-df.columns = new_col_names
-df = df.drop('f0', 1) # first col is irrelevant
-df.head()
-
-
-# ### Find out which columns are all null (THIS SHOULD BE REMOVED)
-
-# In[42]:
-
-[(col, df[col].isnull().sum()) for col in problematic_cols]
-
-
-# ### Reasons to drop a cell
-#  - if the entire column is all null and part of categorical features, then just drop the column as we may imply that that removing that category doesn't do much harm. Further, replacing NAN with 0's is misleading as the entire category should be hashed strings not numbers.
-#   - CHECK THAT YOUR '' IS REPLACED WITH NAN
+# # Fit models and predict
 
 # In[28]:
 
-df_no_NAN = df.dropna(axis=1, how='all')
-df_no_NAN = df_no_NAN.fillna(0)
-df_no_NAN.head()
+model_NB = GaussianNB()
+model_NB.fit(X_train, y_train)
+predicted_NB = model_NB.predict(X_va)
 
+model_NB
 
-# # Visualization
 
 # In[29]:
 
+model_LR = LogisticRegression()
+model_LR.fit(X_train, y_train)
+predicted_LR = model_LR.predict(X_va)
+
+model_LR
+
+
+# In[34]:
+
+model_RF = RandomForestClassifier(n_estimators=10)
+model_RF.fit(X_train, y_train)
+predicted_RF = model_RF.predict(X_va)
+
+model_RF
+
+
+# # ROC curves, Accuracy scores, AUC scores
+
+# In[36]:
+
 get_ipython().magic(u'matplotlib inline')
 
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
+y_predictions = {
+    'Naive Bayes': predicted_NB,
+    'Logistic Regression': predicted_LR,
+    'Random Forest': predicted_RF,
+}
+
+fig, axes = plt.subplots(1, len(y_predictions), sharey=True)
+fig.set_size_inches(15,5)
+i = 0
+
+for title, prediction in y_predictions.items():
+    accuracy = metrics.accuracy_score(y_va, prediction)
+    fulltitle = title + ' (accuracy = %0.2f)' % accuracy
+    fpr, tpr, thresholds = metrics.roc_curve(y_va, prediction, pos_label=1)
+    auc = metrics.auc(fpr, tpr)
+    label = 'AUC = %0.2f' % auc
+
+    axes[i].plot(fpr, tpr, label=label)
+    axes[i].set_title(fulltitle)
+    axes[i].legend(loc='lower right')
+    i += 1
 
 
-# In[30]:
-
-df.head()
-
-
-# ### Plot histogram for all columns
-
-# In[ ]:
-
-histogramCount = 0
-typeErrorCount = 0
-valueErrorCount = 0
-problematic_cols = []
-
-all_columns = list(df.columns.values)
-selected_columns =  all_columns[1:2] + all_columns[24:26]
-for col in selected_columns:
-    try:
-        ax = df_no_NAN[[col]].apply(pd.value_counts).plot(kind='hist', logy='True')
-        ax.set_xlabel("values")
-        ax.set_title("Histogram (col='" + col + "')")
-        histogramCount += 1
-    except TypeError as err:
-        print "Problematic col:" + col
-        print "Error details:", err
-        problematic_cols.append(col)
-        typeErrorCount += 1
-    except ValueError as err:
-        print "Value error for col:" + col
-        print "Error details:", err
-        valueErrorCount += 1
-        
-print "Histogram printed:", histogramCount
-print "TypeError count:", typeErrorCount
-print "ValueError count:", valueErrorCount
-
-
-# # Summary stats for only integer columns
+# # Test 3M
 
 # In[37]:
 
-integer_columns = all_columns[:13]
-df[integer_columns].describe()
+df_test = pd.read_table('test3M.txt', header=None)
+X_test, y_test = get_train_xy(df_test, selected_categ_cols)
 
 
-# # Normalize integer columns after transpose
+# In[38]:
 
-# In[ ]:
-
-df_norm = df[integer_columns].T.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
-df_norm.head()
-
-
-# # Select features to be used
-
-# In[ ]:
-
-cols_to_keep =  
-    all_columns[:13] +  
-    all_columns[18:21] + 
-    ['f22', 'f24', 'f26'] +
-    all_columns[27:31] + 
-    all_columns[33:36]
-    
-cols_to_keep
-
-
-# # One hot encoding
-
-# In[ ]:
-
-#cols_to_keep = all_columns[:13] + all_columns[17:24]
-df_onehot = pd.get_dummies(df[cols_to_keep])
-print df_onehot.shape
-df_onehot.head()
-
-
-# # Model and Evaluation
-
-# In[495]:
-
-X = df_onehot
-y = test_data.index.values
-
-
-# ### Logistic Regression
-
-# In[502]:
-
-from sklearn.linear_mode import LogisticRegression
-model = LogisticRegression()
-model.fit(X,y)
-
-
-# In[505]:
-
-model.score(X,y)
-
-
-# ### Split up the data set into test and training sets
-
-# In[510]:
-
-from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-testLogRModel = LogisticRegression()
-testLogRModel.fit(x_train, y_train)
-
-
-# In[511]:
-
-testLogRModel.score(X,y)
-
-
-# ### Predict class labels for the test set
-
-# In[514]:
-
-predicted = testLogRModel.predict(x_test)
-print predicted
-
-
-# ### AUC score from ROC
-
-# In[517]:
-
-from sklearn import metrics
-print "accuracy:", metrics.accuracy_score(y_test, predicted)
-# print "auc:", metrics.roc_auc_score(y_test, predicted)
-
-
-# ### Perform cross validation using n folds
-
-# In[526]:
-
-from sklearn.cross_validation import cross_val_score
-#n = 5
-#scores = cross_val_score(LogisticRegression(), X, y, scoring='accuracy', cv=n)
-#print scores
-#print scores.mean()
-
-
-# ### Random Forest Classifier
-
-# In[523]:
-
-from sklearn.ensemble import RandomForestClassifier
-clf_random_forest = RandomForestClassifier(n_estimators=10)
-clf_random_forest.fit(X,y)
-
-
-# In[525]:
-
-clf_random_forest.score(X,y)
+predicted_test = model_LR.predict(X_test)
+print "Test3M accuracy:", metrics.accuracy_score(y_test, predicted_test)
+print "Test3M auc:", metrics.roc_auc_score(y_test, predicted_test)
 
 
 # In[ ]:

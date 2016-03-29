064d5b6a1d9fb4f83b0bf1cf353d6284d6c184bd
Fabian Okeke <fnokeke@gmail.com>
2016-03-28 21:30:41 -0400

    assign2

---
+++ assign2/RecSys.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# # Recommendation System
+
+#  - MovieLens 10M data set
+#  - MovieLens 22M data set
+#  - Million song data set
+
+# # Split dataset into 60-20-20 train-validate-test partitions
+
+# In[2]:
+
+# sort by time then split
+
+
+# # Using train data, learn ALS model
+
+# In[ ]:
+
+
+
+
+# # Using validation data, choose different regularization parameters with different latent factors
+
+# In[ ]:
+
+
+
+
+# # Using test data, test chosen model and report metric error
+
+# In[ ]:
+
+
+
